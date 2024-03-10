526b970b00f3397ff6d3e10c5187d50f21cab5d4
SchlexanderOlz <Ihavenonameforthischannel@gmail.com>
2024-03-10 10:03:36 +0100

    Added search bar

--- src/components/Galery.tsx
+++ src/components/Galery.tsx
@@ -117,6 +117,54 @@ const Galery = () => {
       <div className="text-center space-y-3">
         <h1 className="text-5xl text-white font-bold">Galery</h1>
       </div>
+      <div className="flex flex-col items-center">
+        <div className="flex justify-center mt-5">
+          <label className="input input-bordered flex items-center gap-2">
+            <input
+              type="text"
+              className="grow border-none focus:outline-none focus:ring-0"
+              placeholder="Search..."
+            />
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 16 16"
+              fill="currentColor"
+              className="w-4 h-4 opacity-70"
+            >
+              <path
+                fillRule="evenodd"
+                d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </label>
+        </div>
+        <div className="flex mt-4">
+          <label className="form-control w-full max-w-xs">
+            <div className="label">
+              <span className="label-text">From:</span>
+            </div>
+            <input
+              type="date"
+              name="beginTime"
+              placeholder="Timestart"
+              className="input input-secondary input-bordered w-full max-w-xs h-12 mr-3"
+            />
+          </label>
+          <label className="form-control w-full max-w-xs">
+            <div className="label">
+              <span className="label-text">To:</span>
+            </div>
+
+            <input
+              type="date"
+              name="beginTime"
+              placeholder="Timestart"
+              className="input input-secondary input-bordered w-full max-w-xs h-12 ml-3"
+            />
+          </label>
+        </div>
+      </div>
       <div className="w-screen flex justify-center">
         <div className="w-4/5 h-full mt-10 items-center">
           <PhotoAlbum
