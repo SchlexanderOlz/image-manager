0dae670608edf10af72bde7633349a30c9a71f87
SchlexanderOlz <Ihavenonameforthischannel@gmail.com>
2024-03-28 15:37:52 +0100

    Removed uneeded pages route

--- src/pages/api/images/page/[nth].ts
+++
@@ -1,9 +0,0 @@
-import { db } from "@/lib/prisma";
-import { NextApiRequest, NextApiResponse } from "next";
-
-export default async function GET(req: NextApiRequest, res: NextApiResponse) {
-  const nth = Number.parseInt(req.query.nth! as string);
-  const images = await db.getImages();
-
-  res.status(200).json({ images });
-}
